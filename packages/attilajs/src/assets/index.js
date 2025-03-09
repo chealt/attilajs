d7f1dd35e8ce3a9962ca989dc0aae018131b1665
Attila Bartha <atikenny@gmail.com>
2025-03-09 14:15:09 +0100

    separate primary and secondary buttons styles

--- packages/attilajs/src/assets/index.js
+++ packages/attilajs/src/assets/index.js
@@ -1,6 +1,8 @@
 const tag = 'attila-js';
 const mainButtonID = 'main-button';
 const closeButtonID = 'close-button';
+const dataButtonID = 'data-button';
+const buttonPrimaryClassName = 'button--primary';
 const dialogID = 'main-dialog';
 const controlsClassName = 'controls';
 const contentClassName = 'content';
@@ -12,7 +14,9 @@ const css = `
   --font-family: Arial;
   --color-brand: #00703c;
   --color-brand--dark: #005a30;
+  --color-neutral: #f3f3f3;
   --color-highlighted: #fd0;
+  --color-highlighted--neutral: darkgrey;
   --color-text: #0c0c0c;
   --color-brand-text: #fff;
   --gap: 1rem;
@@ -31,8 +35,8 @@ const css = `
 
 button {
   display: inline-block;
-  background-color: var(--color-brand);
-  color: var(--color-brand-text);
+  background-color: var(--color-neutral);
+  color: var(--color-text);
   font-size: var(--font-size);
   padding: var(--gap);
   box-shadow: 0 2px 0 var(--color-text);
@@ -43,10 +47,19 @@ button {
   vertical-align: top;
   cursor: pointer;
   -webkit-appearance: none;
-  text-decoration: none;
+  text-transform: uppercase;
+}
+  
+button.${buttonPrimaryClassName} {
+  background-color: var(--color-brand);
+  color: var(--color-brand-text);
 }
 
 button:hover {
+  background-color: var(--color-highlighted--neutral);
+}
+
+button.${buttonPrimaryClassName}:hover {
   background-color: var(--color-brand--dark);
 }
 
@@ -66,10 +79,6 @@ button:focus:not(:active):not(:hover) {
 #${mainButtonID} {
   font-size: var(--font-size--l);
   padding: var(--gap--l);
-}
-
-#${closeButtonID} {
-  text-transform: uppercase;
 }
 
 .${controlsClassName} {
@@ -117,10 +126,11 @@ class AttilaJS extends HTMLElement {
 
   render() {
     this.shadowRoot.innerHTML = `
-      <button aria-label="Open Attila JS" id="${mainButtonID}">A</button>
+      <button aria-label="Open Attila JS" id="${mainButtonID}" class="${buttonPrimaryClassName}">A</button>
       <dialog id="${dialogID}">
         <div class="${controlsClassName}">
-          <button id="${closeButtonID}">close</button>
+          <button id="${dataButtonID}">data</button>
+          <button id="${closeButtonID}" class="${buttonPrimaryClassName}">close</button>
         </div>
         <div class="${contentClassName}">
           <h1>Attila JS</h1>
